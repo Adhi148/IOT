ad50e46747a1bb82c116f31bf939203512726b37
AzureAD\AdeshGowdaShankarapp <adeshgowda.shankarappa@talentpace.com>
2024-08-08 15:19:30 +0530

    second commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Replace with your desired IP address
+    port: 5000 // Replace with your desired port
+  }
+});
